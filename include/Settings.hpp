005ba14eec9356ebaa66332508997e7c4a5907ee
s3menchuk <160656541+s3menchuk@users.noreply.github.com>
2025-12-24 12:11:56 +0300

    reorganization struct files

---
+++ include/Settings.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "SceneCreator.hpp"
+
+#include <memory>
+#include <numbers>
+#include <string>
+
+namespace Settings {
+const std::string APP_NAME = "Graphics Engine";
+const size_t FRAME_LIMIT = 60;
+
+const float ASPECT = 16.0f / 9.0f;
+const size_t WIDTH = 600;
+const size_t HEIGHT = WIDTH / ASPECT;
+
+const float FOV_Y = std::numbers::pi / 180 * 55;
+
+const float NEAR = 1;
+const float FAR = 1000;
+
+const float MAX_ZENITH_DEGREES = 75;
+const float MAX_ZENITH_RADIANS = std::numbers::pi / 2 * MAX_ZENITH_DEGREES / 90;
+
+const float CAMERA_MOVEMENT_SPEED = 0.1;
+const float CAMERA_ROTATION_SPEED = 0.15;
+
+size_t ray_tracing_depth = 3;
+
+std::shared_ptr<SceneCreator> scene_creator = std::make_shared<ClassicCornellBox>();
+}  // namespace Settings
